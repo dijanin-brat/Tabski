986ada70db5d89cc90ecd53c74f933360f219201
Aleksa Colovic <aleksa.colovic98@gmail.com>
2023-07-24 17:07:55 +0200

    Description for routes

--- routes/post.router.js
+++ routes/post.router.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const PostController = require('../controllers/post.controller');
 const { validatePost } = require('../middlewares/validation');
 
+// get all posts
 router.get('/', PostController.getPosts);
+// get specific post by id
 router.get('/post/:postId', PostController.getPostById);
+// create new post
 router.post('/', validatePost, PostController.createPost);
+// delete post
 router.delete('/post/:postId', PostController.deletePost);
+// update post
 router.patch('/post/:postId', PostController.updatePost);
 
 module.exports = router;
--- routes/user.router.js
+++ routes/user.router.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const UserController = require('../controllers/user.controller');
 const {validateUser, parseToInt} = require('../middlewares/validation')
 
+// get all users
 router.get('/', UserController.getUsers);
+// get user by id
 router.get('/user/:userId', parseToInt, UserController.getUserById);
+// create new user
 router.post('/', validateUser, UserController.createUser);
+// delete user
 router.delete('/user/:userId', parseToInt, UserController.deleteUser);
+// update user
 router.patch('/user/:userId', parseToInt, UserController.updateUser);
+// like specific post for specific user 
 router.post('/liked_post', parseToInt, UserController.likePost);
+// unlike specific post
 router.delete('/liked_post/:likedPostId', parseToInt, UserController.unlikePost);
 
 
